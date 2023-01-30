bca7f50407f217dc0bbc711eb63a6374f7f18fdb
Samir Bagamaev <05ru4315@gmail.com>
2023-01-31 01:06:06 +0300

    WORK_66

---
+++ WORK_66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+//n + SumN(m, n - 1);
+
+int m = 5;
+int n = 69;
+int SumN(int m, int n)
+{
+    if (m == n) return n;
+    else
+    {
+       return n + SumN(m, n - 1);
+    }
+}
+SumN(m , n);
+
+Console.WriteLine($"СУММА ОТ {m} до {n} = {SumN(m, n)}");
