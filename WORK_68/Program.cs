033488062645b885848f464813fb4669f97e5d03
Samir Bagamaev <05ru4315@gmail.com>
2023-01-31 01:59:47 +0300

    WORK_68

---
+++ WORK_68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+
+//Функция Аккермана:///////////////////////////////////////////////
+//                                                               //
+//       {2}   { n + 1                {1}  m = 0                 //          
+//   A(m,n) =  { A(m -1, 1) {4}       {3}  m > 0, n = 0          //
+//       {5}   { A(m - 1,  A(m -1, 1))     m  > 0, n > 0         //
+//                                                               //
+///////////////////////////////////////////////////////////////////
+
+int m = 5;
+int n = 69;
+
+int AkkermansFunction(int m, int n )
+{
+  if (m == 0)
+  return n + 1;
+  
+  if (m > 0 && n == 0 )
+  return AkkermansFunction(m - 1, 1);
+  else 
+  return AkkermansFunction(m - 1,  AkkermansFunction(m - 1, 1));
+}
+AkkermansFunction(m , n);
+Console.WriteLine($"ФУНКЦИЯ АККЕРМАНА ДЛЯ ЗНАЧЕНИЙ {m} и {n} РАВНЯЕТСЯ {AkkermansFunction(m , n)}");
